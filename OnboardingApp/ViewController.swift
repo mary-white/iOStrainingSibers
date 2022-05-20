985f6526706d7dfe4d754bc7395fafe883e8e8b8
Maria Belozerova <mari.beloe@yandex.ru>
2022-05-20 12:11:52 +0700

    Rename variables and functions

--- OnboardingApp/ViewController.swift
+++ OnboardingApp/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 // flying img
 struct FlyingImg {
     var image : UIView? = UIView()
-    var direction : CGPoint = .zero
+    var flyightDirection : CGPoint = .zero
 }
 
 // constant
@@ -19,7 +19,7 @@ let error = 0.5
 class ViewController: UIViewController {
     
     // Views
-    @IBOutlet var startButton : UIButton?
+    @IBOutlet var flyightStartButton : UIButton?
     @IBOutlet var catImg : UIView?
     @IBOutlet var flyingFishNumber : UITextField?
     
@@ -27,34 +27,34 @@ class ViewController: UIViewController {
     @IBOutlet var switchButton : UISwitch?
     
     // Variables - fly image
-    var isFlyState : Bool = false
-    var timerInterval = 0.01
-    var timerFly : Timer?
+    var isFlightState : Bool = false
+    var flightTimerInterval = 0.01
+    var flyightTimer : Timer?
     
     // fly image chaos
-    var flySpeed : CGFloat = 1
+    var flyightSpeed : CGFloat = 1
     
     // fly image circle
-    var radius : CGFloat = 100
+    var circleRadius : CGFloat = 100
     var angle : CGFloat = 0
     
     // flying fish
     var flyingFish : Array<FlyingImg> = Array()
-    let fishHeight : CGFloat = 30
-    let fishWidth : CGFloat = 30
-    let imageNameFish = "fish.jpeg"
+    let fishImgHeight : CGFloat = 30
+    let fishImgWidth : CGFloat = 30
+    let fishImgName = "fish.jpeg"
     
     // flying cat
-    var flyingCat : FlyingImg = FlyingImg(image: nil, direction: .zero)
+    var flyingCat : FlyingImg = FlyingImg(image: nil, flyightDirection: .zero)
     
-    enum FlyButtonText : Int {
-        case start, stop, other
+    enum FlyightButtonText : Int {
+        case flyightStart, flyightStop, otherState
         
         func simpleDescription() -> String {
             switch self {
-            case .start:
+            case .flyightStart:
                 return "start fly"
-            case .stop:
+            case .flyightStop:
                 return "stop fly"
             default:
                 return "Wow"
@@ -65,46 +65,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        startButton?.setTitle(FlyButtonText.start.simpleDescription(), for: .normal)
+        flyightStartButton?.setTitle(FlyightButtonText.flyightStart.simpleDescription(), for: .normal)
         textLabel?.text = "Flying cat"
         flyingCat.image = catImg
     }
 
     @IBAction func didTapStart() {
-        var title : FlyButtonText
-        if !isFlyState {
+        var buttonTitle : FlyightButtonText
+        if !isFlightState {
             // button work
-            title = FlyButtonText.stop
+            buttonTitle = FlyightButtonText.flyightStop
             
             // fly direction init
-            let xRandom : CGFloat = CGFloat.random(in: -1...1) * flySpeed
-            let yRandom : CGFloat = CGFloat.random(in: -1...1) * flySpeed
-            flyingCat.direction = CGPoint(x: xRandom, y: yRandom)
+            let randomXDirection : CGFloat = CGFloat.random(in: -1...1) * flyightSpeed
+            let randomYDirection : CGFloat = CGFloat.random(in: -1...1) * flyightSpeed
+            flyingCat.flyightDirection = CGPoint(x: randomXDirection, y: randomYDirection)
             
             //timer turn on
             if (switchButton?.isOn)! {
                 // chaos
-                timerFly = Timer.scheduledTimer(timeInterval: timerInterval, target: self, selector: #selector(moveImgChaosAll), userInfo: nil, repeats: true)
+                flyightTimer = Timer.scheduledTimer(timeInterval: flightTimerInterval, target: self, selector: #selector(moveAllImgChaotically), userInfo: nil, repeats: true)
             } else {
                 // circle
-                flyingCat.image?.center = CGPoint(x: (flyingCat.image?.superview?.bounds.width)! / 2 + radius - 1, y: (flyingCat.image?.superview?.bounds.height)! / 2)
-                timerFly = Timer.scheduledTimer(timeInterval: timerInterval, target: self, selector: #selector(moveImgCircle), userInfo: nil, repeats: true)
+                flyingCat.image?.center = CGPoint(x: (flyingCat.image?.superview?.bounds.width)! / 2 + circleRadius - 1, y: (flyingCat.image?.superview?.bounds.height)! / 2)
+                flyightTimer = Timer.scheduledTimer(timeInterval: flightTimerInterval, target: self, selector: #selector(moveImgInACircle), userInfo: nil, repeats: true)
             }
         } else {
             // button work
-            title = FlyButtonText.start
+            buttonTitle = FlyightButtonText.flyightStart
             
             //timer turn off
-            timerFly?.invalidate()
-            timerFly = nil
+            flyightTimer?.invalidate()
+            flyightTimer = nil
             
             // delete fish
             clearFishArray()
         }
         
         // change button state
-        startButton?.setTitle(title.simpleDescription(), for: .normal)
-        isFlyState = !isFlyState
+        flyightStartButton?.setTitle(buttonTitle.simpleDescription(), for: .normal)
+        isFlightState = !isFlightState
     }
     
     func clearFishArray() {
@@ -115,26 +115,26 @@ class ViewController: UIViewController {
     }
     
     // image fly
-    @objc func moveImgChaosAll() {
+    @objc func moveAllImgChaotically() {
         // fly cat
-        moveImgChaos(flyingImg: &flyingCat)
+        moveImgChaotically(flyingImg: &flyingCat)
         
         //fly fish
         var deletedFish : [Int] = []
         for i in 0..<flyingFish.count {
-            moveImgChaos(flyingImg: &flyingFish[i])
+            moveImgChaotically(flyingImg: &flyingFish[i])
             // fix collision between cat end fish
-            if isImagesCollision(cat: flyingCat.image!, fish: flyingFish[i].image!) {
+            if isImgCollision(firstImg: flyingCat.image!, secondImg: flyingFish[i].image!) {
                 // change cat direction
-                let differenceX = (flyingCat.image?.center.x)! - (flyingFish[i].image?.center.x)!
-                let differenceY = (flyingCat.image?.center.y)! - (flyingFish[i].image?.center.y)!
+                let differenceXImgCenter = (flyingCat.image?.center.x)! - (flyingFish[i].image?.center.x)!
+                let differenceYImgCenter = (flyingCat.image?.center.y)! - (flyingFish[i].image?.center.y)!
                 
-                if abs(differenceY) < ((flyingCat.image?.bounds.height)! / 2 + (flyingFish[i].image?.bounds.height)! / 2) {
-                    flyingCat.direction.x = (differenceX > 0 ? 1 : -1) * abs(flyingCat.direction.x)
+                if abs(differenceYImgCenter) < ((flyingCat.image?.bounds.height)! / 2 + (flyingFish[i].image?.bounds.height)! / 2) {
+                    flyingCat.flyightDirection.x = (differenceXImgCenter > 0 ? 1 : -1) * abs(flyingCat.flyightDirection.x)
                 }
                 
-                if abs(differenceX) < ((flyingCat.image?.bounds.width)! / 2 + (flyingFish[i].image?.bounds.width)! / 2) {
-                    flyingCat.direction.y = (differenceY > 0 ? 1 : -1) * abs(flyingCat.direction.y)
+                if abs(differenceXImgCenter) < ((flyingCat.image?.bounds.width)! / 2 + (flyingFish[i].image?.bounds.width)! / 2) {
+                    flyingCat.flyightDirection.y = (differenceYImgCenter > 0 ? 1 : -1) * abs(flyingCat.flyightDirection.y)
                 }
                 
                 // delete fish from screen
@@ -148,15 +148,15 @@ class ViewController: UIViewController {
         }
     }
     
-    func moveImgChaos(flyingImg: inout FlyingImg) {
-        flyingImg.image?.center = CGPoint(x: (flyingImg.image?.center.x)! + flyingImg.direction.x, y: (flyingImg.image?.center.y)! + flyingImg.direction.y)
-        fixImgBoundsdCollision(flyingImg: &flyingImg)
+    func moveImgChaotically(flyingImg: inout FlyingImg) {
+        flyingImg.image?.center = CGPoint(x: (flyingImg.image?.center.x)! + flyingImg.flyightDirection.x, y: (flyingImg.image?.center.y)! + flyingImg.flyightDirection.y)
+        pushImgAwayFromScreenBounds(flyingImg: &flyingImg)
     }
     
-    @objc func moveImgCircle() {
-        let coordinateX = (flyingCat.image?.superview?.bounds.width)! / 2 + radius * cos(angle)
-        let coordinateY = (flyingCat.image?.superview?.bounds.height)! / 2 + radius * sin(angle)
-        flyingCat.image?.center = CGPoint(x: coordinateX, y: coordinateY)
+    @objc func moveImgInACircle() {
+        let imgXCoordinate = (flyingCat.image?.superview?.bounds.width)! / 2 + circleRadius * cos(angle)
+        let imgYCoordinate = (flyingCat.image?.superview?.bounds.height)! / 2 + circleRadius * sin(angle)
+        flyingCat.image?.center = CGPoint(x: imgXCoordinate, y: imgYCoordinate)
         
         angle += 0.01
         if angle >= 2 * CGFloat.pi {
@@ -165,57 +165,52 @@ class ViewController: UIViewController {
     }
     
     // fix image boards collisions
-    func fixImgBoundsdCollision(flyingImg: inout FlyingImg) {
+    func pushImgAwayFromScreenBounds(flyingImg: inout FlyingImg) { // rename
         // x bound
         if (flyingImg.image?.frame.minX)! <= error  || (flyingImg.image?.superview?.bounds.width)! - (flyingImg.image?.frame.maxX)! <= error {
-            flyingImg.direction.x = -1 * flyingImg.direction.x
+            flyingImg.flyightDirection.x = -1 * flyingImg.flyightDirection.x
         }
         
         //y bound
         if (flyingImg.image?.frame.minY)! <= error  || (flyingImg.image?.superview?.bounds.height)! - (flyingImg.image?.frame.maxY)! <= error {
-            flyingImg.direction.y = -1 * flyingImg.direction.y
+            flyingImg.flyightDirection.y = -1 * flyingImg.flyightDirection.y
         }
     }
     
     // fix images collision
-    func isImagesCollision(cat: UIView, fish: UIView) -> Bool {
-        // first up/down
-        return abs(fish.center.x - cat.center.x) < (cat.bounds.width + fish.bounds.width) / 2 &&
-            abs(fish.center.y - cat.center.y) < (cat.bounds.height + fish.bounds.height) / 2
+    func isImgCollision(firstImg: UIView, secondImg: UIView) -> Bool {
+        return abs(secondImg.center.x - firstImg.center.x) < (firstImg.bounds.width + secondImg.bounds.width) / 2 &&
+            abs(secondImg.center.y - firstImg.center.y) < (firstImg.bounds.height + secondImg.bounds.height) / 2
     }
     
     // create fish
     @IBAction func createFish() {
-        if isFlyState || !(switchButton?.isOn)!{
+        if isFlightState || !(switchButton?.isOn)!{
             return
         }
 
         // fill using fish
-        let fishNumber : Int? = Int((flyingFishNumber?.text)!)
-        // if not a number
-        if fishNumber == nil {
-            return
-        }
+        let fishNumber : Int = Int((flyingFishNumber?.text)!) ?? 0
         
-        let bounds = flyingCat.image?.superview?.bounds
+        let screenBounds = flyingCat.image?.superview?.bounds
         
-        for _ in 0..<fishNumber! {
+        for _ in 0..<fishNumber {
             var currentFish : FlyingImg = FlyingImg()
             
             // create image
-            let image = UIImage(named: imageNameFish)
-            let imageView = UIImageView(image: image!)
+            let fishImg = UIImage(named: fishImgName)
+            let fishImgView = UIImageView(image: fishImg!)
                 
             //create frame
-            let randomInitX : CGFloat = CGFloat.random(in: (fishWidth / 2)...((bounds?.width)! - fishWidth / 2))
-            let randomInitY : CGFloat = CGFloat.random(in: (fishHeight / 2)...((bounds?.height)! - fishHeight / 2))
-            imageView.frame = CGRect(x: randomInitX, y: randomInitY, width: fishWidth, height: fishHeight)
-            currentFish.image? = imageView
+            let randomInitXFish : CGFloat = CGFloat.random(in: (fishImgWidth / 2)...((screenBounds?.width)! - fishImgWidth / 2))
+            let randomInitYFish : CGFloat = CGFloat.random(in: (fishImgHeight / 2)...((screenBounds?.height)! - fishImgHeight / 2))
+            fishImgView.frame = CGRect(x: randomInitXFish, y: randomInitYFish, width: fishImgWidth, height: fishImgHeight)
+            currentFish.image? = fishImgView
             
             // generate direction
-            let xRandom : CGFloat = CGFloat.random(in: -1...1)
-            let yRandom : CGFloat = CGFloat.random(in: -1...1)
-            currentFish.direction = CGPoint(x: xRandom, y: yRandom)
+            let randomXDirection : CGFloat = CGFloat.random(in: -1...1)
+            let randomYDirection : CGFloat = CGFloat.random(in: -1...1)
+            currentFish.flyightDirection = CGPoint(x: randomXDirection, y: randomYDirection)
             
             // add picture
             flyingCat.image?.superview?.addSubview(currentFish.image!) // add into the frame with cat
