1fb9f7ecc6b3e5e541a0f27e92b19833c8092eb7
Maria Belozerova <mari.beloe@yandex.ru>
2022-05-20 12:56:02 +0700

    Init commit

---
+++ fenecFox/fenecFox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fenecFox
+//
+//  Created by student on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
