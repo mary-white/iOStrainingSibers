49a25b33b5d9bf817647913e71f97414956fb1a5
Maria Belozerova <mari.beloe@yandex.ru>
2022-05-20 13:55:31 +0700

    Add generation of the table context (random, with button)

--- FennecFox/FennecFox/ViewController.swift
+++ FennecFox/FennecFox/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+// constants
+let maxCellNumber = 10
+let maxNumberInCell = 10
+
 class ViewController: UIViewController {
+    
+    @IBOutlet var table : UITableView?
+    @IBOutlet var reloadtableButton : UIButton?
+    
+    // cell variables
+    var cellNumber = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        reloadtableButton?.setTitle("Update the table context", for: .normal)
+        
+        // generate cell number
+        cellNumber = Int.random(in: 1...maxCellNumber)
+        
+        table?.dataSource = self                                                                                 // who cay be?
+        //table.delegate = self                                                                                 // necessary?
+        table?.register(UITableViewCell.self, forCellReuseIdentifier: "standartCell")
+    }
+    
+    @IBAction func updateTableData() {
+        cellNumber = Int.random(in: 1...maxCellNumber)
+        table?.reloadData()
     }
 
+    func generateDataForTable() -> String {
+        return String(Int.random(in: 0...maxNumberInCell))
+    }
+}
 
+// table generation - overload function
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellNumber
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       // Ask for a cell of the appropriate type.
+       let cell = tableView.dequeueReusableCell(withIdentifier: "standartCell", for: indexPath)
+            
+       // Cell content
+        cell.textLabel!.text = generateDataForTable()
+        
+       return cell
+    }
 }
 
