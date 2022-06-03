1692d0d601e6178e727a978db0a96536cf760eb7
Maria Belozerova <mari.beloe@yandex.ru>
2022-06-03 13:00:25 +0700

    Rename variables, clear the code

--- FennecFox/FennecFox/StatisticViewController.swift
+++ FennecFox/FennecFox/StatisticViewController.swift
@@ -39,8 +39,8 @@ class StatisticViewController: UIViewController {
     }
     
     func drawHistogram() {
-        guard let floatWidth = statisticHistogram?.bounds.width,
-                let floatHeight = statisticHistogram?.bounds.height else {
+        guard let floatHistWidth = statisticHistogram?.bounds.width,
+                let floatHistHeight = statisticHistogram?.bounds.height else {
             return
         }
         
@@ -48,60 +48,59 @@ class StatisticViewController: UIViewController {
         let yIndent = 10
         let xIndent = 13
         
-        let width = Int(floatWidth)
-        let height = Int(floatHeight)
+        let histFrameWidth = Int(floatHistWidth)
+        let histFrameHeight = Int(floatHistHeight)
+        let xZero = 2*xIndent
         
         let halfLineLength = 5
         
         let yStepNumber = 10
         
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: histFrameWidth, height: histFrameHeight))
         
-        let barWidth = width - 3*xIndent
-        let barHeight = height - 3*yIndent
+        let histWidth = histFrameWidth - 3*xIndent
+        let histHeight = histFrameHeight - 3*yIndent
         
         // get data
-        let dataForBar = viewModel.statisticDistribution(barNumber: barNumber)
+        let dataForHist = viewModel.statisticDistribution(barNumber: barNumber)
         //print(dataForBar)
+        
+        // steps
+        let xLabelStep : Int = histWidth / barNumber
+        
+        guard let maxHistValue = dataForHist.max() else {
+            return
+        }
+        let yStep : Double = Double(histHeight + yIndent) / (Double(maxHistValue) * 1.1)
+        let yLabelStep : Int = (histHeight + yIndent) / yStepNumber
 
         let img = renderer.image { context in
             // x axis
-            context.cgContext.addLines(between: [CGPoint(x: barWidth + 2*xIndent, y: barHeight), CGPoint(x: 2*xIndent, y: barHeight)])
+            context.cgContext.addLines(between: [CGPoint(x: histWidth + xZero, y: histHeight), CGPoint(x: xZero, y: histHeight)])
             //y axis
-            context.cgContext.addLines(between: [CGPoint(x: 2*xIndent, y: barHeight + yIndent), CGPoint(x: 2*xIndent, y: 0)])
-            
-            // steps
-            let xLabelStep : Int = barWidth / barNumber
-            
-            guard let maxValue = dataForBar.max() else {
-                return
-            }
-            let yStep : Double = Double(barHeight + yIndent) / (Double(maxValue) * 1.1)
-            let yLabelStep : Int = (barHeight + yIndent) / yStepNumber
+            context.cgContext.addLines(between: [CGPoint(x: xZero, y: histHeight + yIndent), CGPoint(x: xZero, y: 0)])
             
             // y label
-            for i in 1...yStepNumber {
-                context.cgContext.addLines(between: [CGPoint(x: 2*xIndent + halfLineLength, y: i*yLabelStep - yIndent), CGPoint(x: 2*xIndent - halfLineLength, y: i*yLabelStep - yIndent)])
-                let yAxisLabel : String = String(i * Int(Double(maxValue) * 1.1) / yStepNumber)
+            for i in 0...yStepNumber {
+                context.cgContext.addLines(between: [CGPoint(x: xZero + halfLineLength, y: i*yLabelStep - yIndent), CGPoint(x: xZero - halfLineLength, y: i*yLabelStep - yIndent)])
                 
-                yAxisLabel.draw(at: CGPoint(x: 0, y: height - i*yLabelStep - Int(3.5*Double(yIndent))), withAttributes: [:])
+                let yAxisLabel : String = String(i * Int(Double(maxHistValue) * 1.1) / yStepNumber)
+                yAxisLabel.draw(at: CGPoint(x: 0, y: histFrameHeight - i*yLabelStep - Int(3.5*Double(yIndent))), withAttributes: [:])
             }
-            let yAxisLabel : String = "0"
-            yAxisLabel.draw(at: CGPoint(x: 0, y: height - Int(3.5*Double(yIndent))), withAttributes: [:])
             
             let xAxisLabel = String(Int((viewModel.dataContainer?.min())!))
-            xAxisLabel.draw(at: CGPoint(x: Int(1.5*Double(xIndent)), y: height - xIndent), withAttributes: [:])
+            xAxisLabel.draw(at: CGPoint(x: Int(1.5*Double(xIndent)), y: histFrameHeight - xIndent), withAttributes: [:])
             for i in 1...barNumber {
                 // x label
-                context.cgContext.addLines(between: [CGPoint(x: i*xLabelStep + 2*xIndent, y: height - 3*yIndent - halfLineLength), CGPoint(x: i*xLabelStep + 2*xIndent, y: height - 3*yIndent + halfLineLength)])
+                context.cgContext.addLines(between: [CGPoint(x: i*xLabelStep + xZero, y: histFrameHeight - 3*yIndent - halfLineLength), CGPoint(x: i*xLabelStep + xZero, y: histFrameHeight - 3*yIndent + halfLineLength)])
                 
                 let xAxisLabel = String(Int((viewModel.dataContainer?.min())!) + i * Int((viewModel.dataContainer?.max())! - (viewModel.dataContainer?.min())!) / barNumber)
-                xAxisLabel.draw(at: CGPoint(x: i*xLabelStep + Int(1.5*Double(xIndent)), y: height - xIndent), withAttributes: [:])
+                xAxisLabel.draw(at: CGPoint(x: i*xLabelStep + Int(1.5*Double(xIndent)), y: histFrameHeight - xIndent), withAttributes: [:])
                 
                 // data
-                let dataSize = Int(Double(dataForBar[i-1]) * yStep)
+                let histDataValue = Int(Double(dataForHist[i-1]) * yStep)
                 context.cgContext.setFillColor(CGColor.init(red: 1, green: 0, blue: 0, alpha: 1))
-                context.cgContext.addRect(CGRect(x: (i-1)*xLabelStep + 2*xIndent, y: barHeight - dataSize, width: xLabelStep, height: dataSize))
+                context.cgContext.addRect(CGRect(x: (i-1)*xLabelStep + xZero, y: histHeight - histDataValue, width: xLabelStep, height: histDataValue))
             }
             
             context.cgContext.drawPath(using: .fillStroke)
