c80f39401131c95ff995d2d330ea083d2b176973
Maria Belozerova <mari.beloe@yandex.ru>
2022-06-08 15:10:16 +0700

    Add converter from JSON to dictionary

--- RestaurantNavigator/RestaurantNavigator/RemoteDataService.swift
+++ RestaurantNavigator/RestaurantNavigator/RemoteDataService.swift
@@ -35,11 +35,64 @@ class RemoteDataService : DataService {
                 return
             }
             
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
+            if let firstData = data, let dataString = String(data: firstData, encoding: .utf8) {
+                print(convertJSONStringToArrayOfDictionaries(dataString))
             }
         }
 
         task.resume()
     }
+}
+
+func parseStringToJSONElements(_ str : String) -> [String] {
+    var result : [String] = []
+    var data = str
+    data.remove(at: data.startIndex)
+    
+    while true {
+        if let firstIndex = data.firstIndex(of: "}") {
+            let dataSecondPart = String(data[data.index(after: firstIndex)..<data.endIndex])
+            if let lastIndex = dataSecondPart.firstIndex(of: "}") {
+                let firstPart = String(data[..<data.index(after: firstIndex)])
+                let secondPart = String(dataSecondPart[..<data.index(after: lastIndex)])
+                let newElement = firstPart + secondPart
+                
+                result.append(newElement)
+                
+                data = String(dataSecondPart[data.index(after: lastIndex)...])
+                data.remove(at: data.startIndex)
+            } else {
+                break
+            }
+        } else {
+            break
+        }
+    }
+    
+    return result
+}
+
+func convertJSONStringToArrayOfDictionaries(_ str : String) -> [[String : Any]] {
+    let arrayOfStrings : [String] = parseStringToJSONElements(str)
+    
+    var result : [[String : Any]] = []
+    
+    for element in arrayOfStrings {
+        result.append(convertJSONStringToDictionary(element))
+    }
+    
+    return result
+}
+
+func convertJSONStringToDictionary(_ str : String) -> [String: Any] {
+    let data = Data(str.utf8)
+    
+    do {
+        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            return json
+        }
+    } catch let error as NSError {
+        print("Failed to load: \(error.localizedDescription)")
+    }
+    return [:]
 }
---
+++ RestaurantNavigator/RestaurantNavigator/Restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant.swift
+//  RestaurantNavigator
+//
+//  Created by student on 08.06.2022.
+//
+
+import Foundation
+import UIKit
+
+class Restaurant {
+    var title : String?
+    var description : String?
+    var galery : [UIImage] = []
+    
+    var reviews : [(user : String, review : String)] = []
+}
