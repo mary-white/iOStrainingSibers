35d93c82f0d9450f095a1b2907796889582c7c71
Maria Belozerova <mari.beloe@yandex.ru>
2022-06-08 11:38:46 +0700

    Add getting url requests

--- RestaurantNavigator/RestaurantNavigator/RemoteDataService.swift
+++ RestaurantNavigator/RestaurantNavigator/RemoteDataService.swift
@@ -7,6 +7,39 @@
 
 import Foundation
 
+let errorStatusCode = [404 : "Bad request", 401 : "Unauthorized", 403 : "Forbidden", 500 : "Internal Server Error", 503 : "Service Unavailable"]
+
 class RemoteDataService : DataService {
     
+    init() {
+        updateDataOfrestaurant()
+    }
+    
+    func updateDataOfrestaurant() {
+        guard let url = URL(string: "https://restaurants-f64d7.firebaseio.com/restaurants.json") else {
+            return
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("URL response error: \(error)")
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse, let responseError = errorStatusCode[response.statusCode] {
+                print("Response HTTP Status code: \(response.statusCode)")
+                print("Response error: " + responseError)
+                return
+            }
+            
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        }
+
+        task.resume()
+    }
 }
