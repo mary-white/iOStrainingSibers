c44147f351e121c60169056afd829c8611af7f7f
Maria Belozerova <mari.beloe@yandex.ru>
2022-06-16 16:56:08 +0700

    Fix UIView size

--- RestaurantNavigator/RestaurantNavigator/UI/PageFlow/RestaurantPageViewController.swift
+++ RestaurantNavigator/RestaurantNavigator/UI/PageFlow/RestaurantPageViewController.swift
@@ -25,12 +25,28 @@ class RestaurantPageViewController: UIViewController, DisplayRestaurantPageViewM
     @IBOutlet var buttonToBookmarkRestaurant : UIButton?
     
     var viewModel : RestaurantPageViewModel?
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let tableHeaderView = tableHeaderView, let tableFooterView = tableFooterView, let reviewsTable = reviewsTable else {
+            return
+        }
+
+        let currentHeaderSize = tableHeaderView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height // tableHeaderView.sizeThatFits(CGSize(width: reviewsTable.frame.width, height: CGFloat.infinity)) //
+        tableHeaderView.frame = CGRect(x:0, y:0, width:reviewsTable.frame.width, height:currentHeaderSize) // CGRect(x:0, y:0, width:currentHeaderSize.width, height:currentHeaderSize.height) //
+
+        let currentFooterSize = tableFooterView.sizeThatFits(CGSize(width: reviewsTable.frame.height, height: CGFloat.infinity))
+        tableFooterView.frame = CGRect(x:0, y:0, width:currentFooterSize.width, height:currentFooterSize.height)
+        
+        reviewsTable.tableHeaderView = tableHeaderView
+        reviewsTable.tableFooterView = tableFooterView
+        reviewsTable.updateConstraintsIfNeeded()
+        reviewsTable.layoutIfNeeded()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // tune table
-        reviewsTable?.tableHeaderView = tableHeaderView
-        reviewsTable?.tableFooterView = tableFooterView
         
         reviewsTable?.delegate = self
         reviewsTable?.dataSource = self
